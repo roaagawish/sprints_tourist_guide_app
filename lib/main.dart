db0c0dda020cce634733f3c0576705476e8c65ab
roaa gawish <roaa.ayman.mail@gmail.com>
2025-01-09 04:02:35 +0200

    intialize easy_localization packege

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'resourses/routes_manager.dart';
 import 'resourses/theme_manager.dart';
 
-void main() {
-  runApp(const MyApp());
+void main()  async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+  runApp(
+    EasyLocalization(
+      supportedLocales: [Locale('en', 'US'), Locale('ar', 'EG')],
+      path: 'assets/lang', 
+      fallbackLocale: Locale('en', 'US'),
+      child: MyApp()
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -12,6 +23,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       title: 'sprint tourist guide Demo',
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.light,
