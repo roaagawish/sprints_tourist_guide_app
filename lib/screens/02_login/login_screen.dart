902e387798c28aafdd6bcd4103205f5d5e6791a1
roaa gawish <roaa.ayman.mail@gmail.com>
2025-01-10 20:40:32 +0200

    add imports

--- lib/screens/02_login/login_screen.dart
+++ lib/screens/02_login/login_screen.dart
@@ -3,6 +3,8 @@ import 'package:fluttertoast/fluttertoast.dart';
 import '../../models/user_model.dart';
 import '../../resourses/colors_manager.dart';
 import '../../resourses/routes_manager.dart';
+import 'widgets/locale_dropdown.dart';
+import 'package:easy_localization/easy_localization.dart';
 
 class LoginScreen extends StatefulWidget {
   final void Function(String languageCode, String? countryCode)? localeChangeCallback;
@@ -19,12 +21,109 @@ class _LoginScreenState extends State<LoginScreen> {
   final _emailAddressController = TextEditingController();
   final _passwordController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
+  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text('login Screen'),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(context.tr('loginPageTitle')),
+        actions: [
+          LocaleDropdown(
+            // dropdown menu to set app locale
+            callback: widget.localeChangeCallback,
+          ),
+          SizedBox(
+            // dummy padding box
+            width: 20,
+          )
+        ],
+      ),
+      body: Center(
+        child: Form(
+            // the user input form
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                // Email field
+                TextFormField(
+                  validator: (String? emailAddress) {
+                    if (emailAddress == null || emailAddress.isEmpty) {
+                      return context.tr('emailAddressEmptyMessage');
+                    }
+                    var valid = emailAddress.contains('@');
+                    if (!valid) {
+                      return context.tr('emailAddressInvalidMessage');
+                    }
+                    return null;
+                  },
+                  controller: _emailAddressController,
+                  decoration: InputDecoration(
+                    hintText: context.tr('emailHint'),
+                  ),
+                ),
+                // Password field
+                TextFormField(
+                  obscureText: _hidePassword,
+                  validator: (String? password) {
+                    if (password == null || password.isEmpty) {
+                      return context.tr('passwordEmptyMessage');
+                    }
+
+                    if (password.length < 6) {
+                      return context.tr('passwordTooShortMessage');
+                    }
+
+                    return null;
+                  },
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                      hintText: context.tr('passwordHint'),
+                      suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _hidePassword = !_hidePassword;
+                            });
+                          },
+                          icon: Icon(_hidePassword
+                              ? Icons.visibility_off
+                              : Icons.visibility))),
+                ),
+                // Sign In button
+                ElevatedButton(
+                  onPressed: () {
+                    var validationResult = _formKey.currentState!.validate();
+                    if (!validationResult) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                            content: Text(context.tr('signInValidationFailureMessage'))),
+                      );
+                      return;
+                    }
+
+                    // place sign-in query here
+
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            content: Text(context.tr('signInSuccessMessage')),
+                            actions: [
+                              TextButton(
+                                  onPressed: () {
+                                    widget.signInSuccessfulCallback?.call();
+                                  },
+                                  child: Text(
+                                      context.tr('close'))),
+                            ],
+                          );
+                        });
+                  },
+                  child: Text(context.tr('login')),
+                ),
+              ],
+            )),
       ),
     );
   }
