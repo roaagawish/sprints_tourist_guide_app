48fe220ceb6bce840b6ca8de70c29fe9254273db
yara30999 <yaranaserelniny@gmail.com>
2025-01-24 20:02:03 +0200

    fix import issue

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:sprints_tourist_guide_app/main.dart';
+import 'package:sprints_tourist_guide_app/app/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
