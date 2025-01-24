287f73f96910aecfabd4b9aa921e2326083b7443
yara30999 <yaranaserelniny@gmail.com>
2025-01-24 23:28:35 +0200

    start using bloc observer for smooth debugging process.

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'app/app_prefs.dart';
 import 'app/my_app.dart';
 import 'resourses/language_manager.dart';
 import 'screens/02_home/blocs/theme_bloc/theme_bloc.dart';
+import 'simple_observer.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,6 +16,7 @@ void main() async {
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
+  Bloc.observer = SimpleObserver();
   runApp(
     EasyLocalization(
         supportedLocales: [
